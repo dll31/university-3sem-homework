e1f878b88f60f92cd0f6a3d47f710bf4343b9baa
dll31 <danila.litvinov31@yandex.ru>
2021-11-20 19:55:52 +0300

    Исправлен main

--- string-searching_algorithms/main.cpp
+++ string-searching_algorithms/main.cpp
@@ -7,37 +7,13 @@
 
 int main()
 {
-    //right solution: 57, 80, 185.
-    std::string haystack = "One of the use cases of pointers to const elements is as function parameters: a function that takes a pointer to non-const as parameter can modify the value passed as argument, while a function that takes a pointer to const as parameter cannot.";
-    std::string needle = "func";
-
     db database = db("database.txt");
+    algorithmsContainer algs;
 
-    algorithm alg;
-    alg.name = "simple search";
-    alg.alg = simple_Algorithm;
-
-    algorithms algs;
-    algs.allAlgs.push_back(alg);
+    algs.addAlgorithm("simple search", simple_Algorithm);
 
     database.loop(algs);
 
-
-    int x = 0;
-    //outputData data = simple_Algorithm(needle, haystack);
-    
-    /*if (data.errors.empty())
-    {
-        for (auto i : data.id)
-        {
-            std::cout << i << '\n';
-        }
-    }
-    else
-    {
-        std::cout << data.errors[0];
-    }*/
-    
 }
 
 
