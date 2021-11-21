a9cce65013d63fda53ca970d930ada0ec089198c
ShamilSafin <90335705+ShamilSafin@users.noreply.github.com>
2021-11-21 16:19:40 +0300

    создание вектора, где храниться длина равных между собой префиксов и суффиксов

---
+++ algoritm_KMP/algoritm_KMP.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <vector> 
+
+
+using namespace std;
+
+string::size_type KMP(const string& S, const string& pattern)
+{
+	vector<int> pf(pattern.length());
+
+	pf[0] = 0;
+	for (int k = 0, i = 1; i < pattern.length(); ++i)
+	{
+		if (pattern[i] == pattern[k])
+		{
+			k++;
+			pf[i] = k;
+		}
+		else if (k == 0)
+		{
+			pf[i] = 0;
+		}
+		else
+		{
+			k = pf[k - 1];
+		}
+	}
+}
+
+int main()
+{
+	string pattern = "st";
+	string ss = "testovastroka";
+	KMP(ss, pattern);
+
+	return 0;
+}
