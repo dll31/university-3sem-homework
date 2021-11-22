4f609e402b16e59076e827edb24b8a9a6651b3f9
leksminure <91364694+leksminure@users.noreply.github.com>
2021-11-22 16:29:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,43 +10,44 @@ struct outputData
     std::vector<int> errors;
 };
 
-outputData rabinKarp(std::string& pattern, std::string text, int q) 
+outputData rabinKarp(std::string& pattern, std::string text) 
 {
   outputData out;
-  int m = pattern.length();
-  int n = text.length();
+  int prime_number = 13;
+  int len_of_pattern = pattern.length();
+  int len_of_text = text.length();
   int i, j;
-  int p = 0;
-  int t = 0;
+  int hash_for_pattern = 0;
+  int hash_for_txt = 0;
   int h = 1;
 
-  for (i = 0; i < m - 1; i++)
-    h = (h * d) % q;
+  for (i = 0; i < len_of_pattern - 1; i++)
+    h = (h * d) % prime_number;
 
-  for (i = 0; i < m; i++) {
-    p = (d * p + pattern[i]) % q;
-    t = (d * t + text[i]) % q;
+  for (i = 0; i < len_of_pattern; i++) {
+    hash_for_pattern = (d * hash_for_pattern + pattern[i]) % prime_number;
+    hash_for_txt = (d * hash_for_txt + text[i]) % prime_number;
   }
 
-  for (i = 0; i <= n - m; i++) {
-    if (p == t) {
-      for (j = 0; j < m; j++) {
+  for (i = 0; i <= len_of_text - len_of_pattern; i++) {
+    if (hash_for_pattern == hash_for_txt) {
+      for (j = 0; j < len_of_pattern; j++) {
         if (text[i + j] != pattern[j])
           break;
       }
 
-      if (j == m)
+      if (j == len_of_pattern)
       {
         out.id.push_back(i);
         break;
       }
     }
 
-    if (i < n - m) {
-      t = (d * (t - text[i] * h) + text[i + m]) % q;
+    if (i < len_of_text - len_of_pattern) {
+      hash_for_txt = (d * (hash_for_txt - text[i] * h) + text[i + len_of_pattern]) % prime_number;
 
-      if (t < 0)
-        t = (t + q);
+      if (hash_for_txt < 0)
+        hash_for_txt = (hash_for_txt + prime_number);
     }
   }
   if (out.id.empty())
