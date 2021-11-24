e5d47ab2de7f396b85bff35fc80accb68ed06c9b
maxim-ctr <markisss75@gmail.com>
2021-11-24 09:19:49 +0300

    Sunday algoritm
    Алгоритм работает исправно, готов к работе и проверке.

---
+++ string-searching_algorithms/Sunday.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "utility.h"
+
+outputData Sunday(std::string& needle, std::string haystack);
--- string-searching_algorithms/main.cpp
+++ string-searching_algorithms/main.cpp
@@ -5,20 +5,21 @@
 #include "simple_algorithm.h"
 #include "RabinKarp.h"
 #include "KMP.h"
+#include "Sunday.h"
 
 
 int main()
 {
     db database = db("database.txt", "outdata.csv");
-    database.numberNeedlesForOneHaystack = 100;
-    database.numberRepeatValue = 10;
+    database.numberNeedlesForOneHaystack = 10;
+    database.numberRepeatValue = 1;
 
     algorithmsContainer algs;
 
     algs.addAlgorithm("simple search", simple_Algorithm);
     algs.addAlgorithm("Rabin-Karp", rabinKarp);
     algs.addAlgorithm("Knuth-Morris-Pratt", Algroithm_KMP);
-
+	algs.addAlgorithm("Sunday", Sunday);
     database.loop(algs);
 
 }
